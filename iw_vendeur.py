f4c3d10168cc126cd90c82a720079369e01befd3
MathieuLPL <lepuilmathieu@gmail.com>
2022-03-03 16:12:59 +0100

    Ajout d'une raison de refus de candidature ou d'entretien

--- iw_vendeur.py
+++ iw_vendeur.py
@@ -12,37 +12,45 @@ from discord_slash.utils.manage_components import *
 from discord_components import *
 from discord_slash import cog_ext
 
+
 class Iw_vendeur(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name = "iw_vendeur", description = "Accepte ou refuse un utilisateur dans l'équipe des vendeurs.")
-    @commands.has_permissions(manage_roles = True)
-    async def iw_vendeur(self, ctx, status, user : discord.User):
+    @cog_ext.cog_slash(name="iw_vendeur", description="Accepte ou refuse un utilisateur dans l'équipe des vendeurs.")
+    @commands.has_permissions(manage_roles=True)
+    async def iw_vendeur(self, ctx, status, user: discord.User, *, reason="Aucune raison n'a été renseignée !"):
         guild = ctx.guild
         if status == "accept":
-            emuser = discord.Embed(title = "Félicitations 🎉✨", description = "Vous êtes **accepté dans l'équipe** des **Vendeurs du ScaryShop**. Vous pouvez commencer à découvrir vos accès sur le serveur Staff du ScaryShop. \n \n > ***Lien du serveur :***  https://discord.gg/S5dwdD97Cr \n \n Nous vous invitions à mentionner le responsable qui vous a fait passé votre entretien afin de prendre un rendez-vous pour les explications. \n \n **Bienvenue dans l'équipe !**", timestamp=datetime.datetime.utcnow(), color = 0xFFA500)
+            emuser = discord.Embed(title="Félicitations 🎉✨",
+                                   description="Vous êtes **accepté dans l'équipe** des **Vendeurs du ScaryShop**. Vous pouvez commencer à découvrir vos accès sur le serveur Staff du ScaryShop. \n \n > ***Lien du serveur :***  https://discord.gg/S5dwdD97Cr \n \n Nous vous invitions à mentionner le responsable qui vous a fait passé votre entretien afin de prendre un rendez-vous pour les explications. \n \n **Bienvenue dans l'équipe !**",
+                                   timestamp=datetime.datetime.utcnow(), color=0xFFA500)
             emuser.set_footer(icon_url=guild.icon_url, text=f"{guild.name}")
 
-            emserver = discord.Embed(description = f"Le message de validation de l'entretien de {user.mention} a bien été envoyé 📧.", timestamp=datetime.datetime.utcnow(), color = 0xFFA500)
+            emserver = discord.Embed(
+                description=f"Le message de validation de l'entretien de {user.mention} a bien été envoyé 📧.",
+                timestamp=datetime.datetime.utcnow(), color=0xFFA500)
             emserver.set_footer(icon_url=guild.icon_url, text=f"{guild.name}")
 
-            await user.send(embed = emuser)
-            await ctx.send(embed = emserver)
+            await user.send(embed=emuser)
+            await ctx.send(embed=emserver)
 
         elif status == "decline":
-            emuser = discord.Embed(title = "Désolé 🎉✨", description = "Malheureusement, votre entretien vendeur **ne donnera pas de suite**. Vous pourrez tout de même retenter votre chance dans 2 mois. \n \n **Bon courage pour la suite !**", timestamp=datetime.datetime.utcnow(), color = 0xFFA500)
+            emuser = discord.Embed(title="Désolé 🥺",
+                                   description=f"Malheureusement, votre entretien vendeur **ne donnera pas de suite**. Vous pourrez tout de même retenter votre chance dans 2 mois. \n \n > *Raison :* {reason} \n \n **Bon courage pour la suite !**",
+                                   timestamp=datetime.datetime.utcnow(), color=0xFFA500)
             emuser.set_footer(icon_url=guild.icon_url, text=f"{guild.name}")
 
-            emserver = discord.Embed(description = f"Le message de refus de l'entretien de {user.mention} a bien été envoyé 📧.", timestamp=datetime.datetime.utcnow(), color = 0xFFA500)
+            emserver = discord.Embed(
+                description=f"Le message de refus de l'entretien de {user.mention} a bien été envoyé 📧.",
+                timestamp=datetime.datetime.utcnow(), color=0xFFA500)
             emserver.set_footer(icon_url=guild.icon_url, text=f"{guild.name}")
 
-            await user.send(embed = emuser)
-            await ctx.send(embed = emserver)
+            await user.send(embed=emuser)
+            await ctx.send(embed=emserver)
 
         else:
             return
-
 
 
 def setup(bot):
--- rc_vendeur.py
+++ rc_vendeur.py
@@ -12,37 +12,45 @@ from discord_slash.utils.manage_components import *
 from discord_components import *
 from discord_slash import cog_ext
 
+
 class Rc_vendeur(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name = "rc_vendeur", description = "Accepte ou refuse un utilisateur en entretien vendeur.")
-    @commands.has_permissions(manage_roles = True)
-    async def rc_vendeur(self, ctx, status, user : discord.User):
+    @cog_ext.cog_slash(name="rc_vendeur", description="Accepte ou refuse un utilisateur en entretien vendeur.")
+    @commands.has_permissions(manage_roles=True)
+    async def rc_vendeur(self, ctx, status, user: discord.User, *, reason="Aucune raison n'a été renseignée !"):
         guild = ctx.guild
         if status == "accept":
-            emuser = discord.Embed(title = "Félicitations 🎉✨", description = "Votre candidature pour devenir vendeur sur le ScaryShop a été **retenue**. Celle-ci donne donc suite à un entretien vocal qui aura lieu sur le serveur discord réservé au Staff du ScaryShop. Si vous partagez le serveur à qui que ce soit, vous serez banni de nos services. \n \n > ***Lien du serveur :***  https://discord.gg/S5dwdD97Cr \n \n Lorsque vous arrivez dessus, veuillez indiquer dans #ma-demande la raison pour laquelle vous figurez sur le serveur. \n \n **Bon courage pour l'entretien !**", timestamp=datetime.datetime.utcnow(), color = 0xFFA500)
+            emuser = discord.Embed(title="Félicitations 🎉✨",
+                                   description="Votre candidature pour devenir vendeur sur le ScaryShop a été **retenue**. Celle-ci donne donc suite à un entretien vocal qui aura lieu sur le serveur discord réservé au Staff du ScaryShop. Si vous partagez le serveur à qui que ce soit, vous serez banni de nos services. \n \n > ***Lien du serveur :***  https://discord.gg/S5dwdD97Cr \n \n Lorsque vous arrivez dessus, veuillez indiquer dans #ma-demande la raison pour laquelle vous figurez sur le serveur. \n \n **Bon courage pour l'entretien !**",
+                                   timestamp=datetime.datetime.utcnow(), color=0xFFA500)
             emuser.set_footer(icon_url=guild.icon_url, text=f"{guild.name}")
 
-            emserver = discord.Embed(description = f"Le message de validation de la candidature de {user.mention} a bien été envoyé 📧.", timestamp=datetime.datetime.utcnow(), color = 0xFFA500)
+            emserver = discord.Embed(
+                description=f"Le message de validation de la candidature de {user.mention} a bien été envoyé 📧.",
+                timestamp=datetime.datetime.utcnow(), color=0xFFA500)
             emserver.set_footer(icon_url=guild.icon_url, text=f"{guild.name}")
 
-            await user.send(embed = emuser)
-            await ctx.send(embed = emserver)
+            await user.send(embed=emuser)
+            await ctx.send(embed=emserver)
 
         elif status == "decline":
-            emuser = discord.Embed(title = "Désolé 🎉✨", description = "Malheureusement, votre candidature pour devenir vendeur sur le ScaryShop **n'a pas été retenue**. Vous pourrez tout de même retenter votre chance dans 2 mois. \n \n **Bon courage pour la suite !**", timestamp=datetime.datetime.utcnow(), color = 0xFFA500)
+            emuser = discord.Embed(title="Désolé 🥺",
+                                   description=f"Malheureusement, votre candidature pour devenir vendeur sur le ScaryShop **n'a pas été retenue**. Vous pourrez tout de même retenter votre chance dans 2 mois. \n \n > *Raison :* {reason} \n \n **Bon courage pour la suite !**",
+                                   timestamp=datetime.datetime.utcnow(), color=0xFFA500)
             emuser.set_footer(icon_url=guild.icon_url, text=f"{guild.name}")
 
-            emserver = discord.Embed(description = f"Le message de refus de la candidature de {user.mention} a bien été envoyé 📧.", timestamp=datetime.datetime.utcnow(), color = 0xFFA500)
+            emserver = discord.Embed(
+                description=f"Le message de refus de la candidature de {user.mention} a bien été envoyé 📧.",
+                timestamp=datetime.datetime.utcnow(), color=0xFFA500)
             emserver.set_footer(icon_url=guild.icon_url, text=f"{guild.name}")
 
-            await user.send(embed = emuser)
-            await ctx.send(embed = emserver)
+            await user.send(embed=emuser)
+            await ctx.send(embed=emserver)
 
         else:
             return
-
 
 
 def setup(bot):
